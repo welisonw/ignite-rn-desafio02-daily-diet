b0ebd53c829da6c19a6cdda47de6f2eaf5da43be
Welison Wessling <wwelisonw@gmail.com>
2023-12-01 16:36:43 -0300

    feat: created themes

---
+++ src/themes/index.ts
@@ -0,0 +1,5 @@
+
+import { colors } from "@/themes/src/colors";
+import { fonts } from "@/themes/src/fonts";
+
+export default { colors, fonts };
---
+++ src/themes/src/fonts.ts
@@ -0,0 +1,19 @@
+export const fonts = {
+  fontFamilys: {
+    regular: "NunitoSans_400Regular",
+    bold: "NunitoSans_700Bold",
+  },
+
+  fontSizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24,
+    "2xl": 32
+  },
+
+  lineHeights: {
+    md: "130%",
+  },
+};
