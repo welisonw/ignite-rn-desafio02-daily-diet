e91838a6e43f52ae56926bdbfd27c26c21b8adf7
Welison Wessling <wwelisonw@gmail.com>
2023-12-02 11:30:18 -0300

    refactor: adjusted color name component Loading

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -4,10 +4,10 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme }) => theme.colors.base.gray_700};
+  background-color: ${({ theme }) => theme.colors.base.gray_100};
   `;
 
 export const LoadIndicator = styled.ActivityIndicator.attrs(({ theme }) => ({
-  color: theme.colors.base.gray_200,
+  color: theme.colors.base.gray_600,
   size: "large",
 }))``;
