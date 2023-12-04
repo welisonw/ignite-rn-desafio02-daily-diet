f3bdc71e53630d06290fa469b559c51eaf845684
Welison Wessling <wwelisonw@gmail.com>
2023-12-04 10:49:03 -0300

    feat: created arrows

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,53 @@
-import { CardBgColors, Container } from "./styles";
+import {
+  CardBgColors,
+  Container,
+  ContainerArrowBack,
+  ContainerArrowUpRight,
+  IconArrowBack,
+  IconArrowUpRight,
+} from "./styles";
 import { Texts } from "@/components/Texts";
 import { ViewProps } from "react-native";
 
-interface CardProps extends CardBgColors, ViewProps {
+interface CardProps extends CardBgColors, ButtonFloatProps, ViewProps {
   title: string;
   subtitle: string;
-};
+  showIconButton?: boolean;
+  onPress?: () => void;
+}
 
-export const Card = ({ bgColor = "gray_200", title, subtitle, ...props }: CardProps) => {
+interface ButtonFloatProps {
+  iconPosition?: "left" | "right";
+}
+
+export const Card = ({
+  bgColor = "gray",
+  iconPosition = "left",
+  showIconButton = true,
+  onPress,
+  title,
+  subtitle,
+  ...props
+}: CardProps) => {
   return (
-    <Container bgColor={bgColor} {...props} >
-      <Texts fontSize="2xl" fontFamily="bold">{title}</Texts>
-      <Texts fontSize="sm" color="gray_600">{subtitle}</Texts>
+    <Container bgColor={bgColor} {...props}>
+      {showIconButton &&
+        (iconPosition === "left" ? (
+          <ContainerArrowBack onPress={onPress}>
+            <IconArrowBack bgColor={bgColor} />
+          </ContainerArrowBack>
+        ) : (
+          <ContainerArrowUpRight onPress={onPress}>
+            <IconArrowUpRight bgColor={bgColor} />
+          </ContainerArrowUpRight>
+        ))}
+
+      <Texts fontSize="2xl" fontFamily="bold">
+        {title}
+      </Texts>
+      <Texts fontSize="sm" color="gray_600">
+        {subtitle}
+      </Texts>
     </Container>
   );
 };
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,18 +1,48 @@
 import styled from "styled-components/native";
+import { ArrowUpRight, ArrowLeft } from "phosphor-react-native"; 
 
 export interface CardBgColors {
-  bgColor?: "gray_200" | "green_light" | "red_light";
+  bgColor?: "gray" | "green" | "red";
 };
 
 export const Container = styled.View<CardBgColors>`
+  position: relative;
   align-items: center;
   justify-content: center;
   height: 102px;
   border-radius: 8px;
   background-color: ${({ theme, bgColor }) => {
-    if (bgColor === "gray_200") return theme.colors.base.gray_200;
-    if (bgColor === "green_light") return theme.colors.brand.green_light;
-    if (bgColor === "red_light") return theme.colors.brand.red_light;
+    if (bgColor === "gray") return theme.colors.base.gray_200;
+    if (bgColor === "green") return theme.colors.brand.green_light;
+    if (bgColor === "red") return theme.colors.brand.red_light;
     else return theme.colors.base.gray_200;
   }};
 `;
+
+export const ContainerArrowUpRight = styled.TouchableOpacity`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 8px;
+`;
+
+export const IconArrowUpRight = styled(ArrowUpRight).attrs<CardBgColors>(({ theme, bgColor }) => ({
+  size: 24,
+  color: bgColor === "green" ? theme.colors.brand.green_dark
+       : bgColor === "red" ? theme.colors.brand.red_dark 
+       : theme.colors.base.gray_700,
+}))<CardBgColors>``;
+
+export const ContainerArrowBack = styled.TouchableOpacity`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 8px;
+`;
+
+export const IconArrowBack = styled(ArrowLeft).attrs<CardBgColors>(({ theme, bgColor }) => ({
+  size: 24,
+  color: bgColor === "green" ? theme.colors.brand.green_dark
+       : bgColor === "red" ? theme.colors.brand.red_dark 
+       : theme.colors.base.gray_700,
+}))<CardBgColors>``;
