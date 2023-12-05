cffed1d460598fb49c8ade8b11cbe1dea791073f
Welison Wessling <wwelisonw@gmail.com>
2023-12-05 14:08:26 -0300

    refactor: ajusted extends TextsProps

--- src/components/Texts/index.tsx
+++ src/components/Texts/index.tsx
@@ -1,7 +1,8 @@
 import { PropsWithChildren } from "react";
 import { Container, TextsStylesProps } from "./styles";
+import { TextProps } from "react-native";
 
-interface TextsProps extends PropsWithChildren, TextsStylesProps {};
+interface TextsProps extends TextProps, TextsStylesProps {};
 
 export const Texts = ({ children, ...props }: TextsProps) => {
   return (
