2074bf2ca26d80b443db4512a1cb91ea106d88f1
Welison Wessling <wwelisonw@gmail.com>
2023-12-14 17:22:18 -0300

    styles: ajusted formatting

--- src/components/Meal/index.tsx
+++ src/components/Meal/index.tsx
@@ -1,19 +1,34 @@
-import { Container, Divider, IconStatus, MealStatusStylesProps } from "./styles";
 import { TouchableOpacityProps } from "react-native";
+import {
+  Container,
+  Divider,
+  IconStatus,
+  MealStatusStylesProps,
+} from "./styles";
 import { Texts } from "@/components/Texts";
 
 interface MealProps extends MealStatusStylesProps, TouchableOpacityProps {
   id: string;
   hour: string;
-  description: string;
+  name: string;
 };
 
-export const Meal = ({ id, hour, description, isOnTheDiet, ...props }: MealProps) => {
+export const Meal = ({
+  id,
+  name,
+  hour,
+  isOnTheDiet,
+  ...props
+}: MealProps) => {
   return (
     <Container id={id} {...props}>
-      <Texts fontFamily="bold" fontSize="xs">{hour}</Texts>
+      <Texts fontFamily="bold" fontSize="xs">
+        {hour}
+      </Texts>
       <Divider />
-      <Texts color="gray_600" style={{ flex: 1 }}>{description}</Texts>
+      <Texts color="gray_600" style={{ flex: 1 }}>
+        {name}
+      </Texts>
       <IconStatus isOnTheDiet={isOnTheDiet} />
     </Container>
   );
