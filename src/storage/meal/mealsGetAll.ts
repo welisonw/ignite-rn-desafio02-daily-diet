f9354f5f349aeccc5cbce1e8f60ca6ebe688ea47
Welison Wessling <wwelisonw@gmail.com>
2023-12-14 17:27:20 -0300

    feat: add meal order logic

--- src/storage/meal/mealsGetAll.ts
+++ src/storage/meal/mealsGetAll.ts
@@ -7,8 +7,15 @@ export const mealsGetAll = async () => {
   try {
     const storage = await AsyncStorage.getItem(MEAL_COLLECTION);
 
-    const meals: MealDTO[] = storage ? JSON.parse(storage) : [];
-    
+    let meals: MealDTO[] = storage ? JSON.parse(storage) : [];
+
+    meals = meals.sort((a, b) => {      
+      const dateA = new Date(+a.date.split('/')[2], +a.date.split('/')[1], +a.date.split('/')[0]);
+      const dateB = new Date(+b.date.split('/')[2], +b.date.split('/')[1], +b.date.split('/')[0]);
+
+      return dateB.getTime() - dateA.getTime();
+    });
+
     return meals;
   } catch (error) {
     throw error;
