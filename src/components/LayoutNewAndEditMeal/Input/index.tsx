eed91db202144f12647c50f005e26c0a54387a79
Welison Wessling <wwelisonw@gmail.com>
2023-12-14 17:17:09 -0300

    refactor: adjusted date and time formatting of the Input component

--- src/components/LayoutNewAndEditMeal/Input/index.tsx
+++ src/components/LayoutNewAndEditMeal/Input/index.tsx
@@ -1,18 +1,38 @@
-import { Texts } from "@/components/Texts";
 import { Container, InputText, inputTextStylesProps } from "./styles";
 import { TextInputProps } from "react-native";
+import { Texts } from "@/components/Texts";
+import { dateMask, hourMask } from "@/utils/mask";
 
 interface InputProps extends inputTextStylesProps, TextInputProps {
   label: string;
+  mask?: "date" | "hour";
+  onInputMaksChange?: (text: string) => void;
 }
 
-export const Input = ({ label, multiline = false, ...props }: InputProps) => {
+export const Input = ({
+  label,
+  multiline = false,
+  mask,
+  onInputMaksChange,
+  ...props
+}: InputProps) => {
+  function handleChange(text: string) {
+    onInputMaksChange && mask === "date" && onInputMaksChange(dateMask(text));
+    onInputMaksChange && mask === "hour" && onInputMaksChange(hourMask(text));
+  };
+
   return (
-    <Container {...props}>
+    <Container >
       <Texts fontFamily="bold" fontSize="sm" color="gray_600">
         {label}
       </Texts>
-      <InputText multiline={multiline} />
+
+      <InputText
+        multiline={multiline}
+        maxLength={mask === "date" ? 10 : mask === "hour" ? 5 : 50}
+        onChangeText={(text) => handleChange(text)}
+        {...props}
+      />
     </Container>
   );
 };
--- src/components/LayoutNewAndEditMeal/Input/styles.ts
+++ src/components/LayoutNewAndEditMeal/Input/styles.ts
@@ -9,11 +9,11 @@ export const Container = styled.View`
   gap: 5px;
 `;
 
-export const InputText = styled.TextInput<inputTextStylesProps>` 
+export const InputText = styled.TextInput<inputTextStylesProps>`
   height: ${({ multiline }) => (multiline ? "120px" : "48px")};
   padding: 14px;
   margin-bottom: 24px;
   font-family: ${({ theme }) => theme.fonts.fontFamilys.regular};
   border: 1px solid ${({ theme }) => theme.colors.base.gray_300};
   border-radius: 6px;
-  `;
+`;
